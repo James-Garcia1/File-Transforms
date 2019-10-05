f1aa0b903107e30d34e98ee3059c50f1d4cbd686
James G Garcia <jimmygus22@gmail.com>
2019-10-05 05:54:24 -0500

    Update Database.cpp
    moved functions to separate file.

--- Database.cpp
+++ Database.cpp
@@ -1,3 +1,4 @@
+#include "functions.h"
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -6,49 +7,13 @@
 
 using namespace std;
 
-int sortfile(char* fileName) {
-    ifstream ifs(fileName);
-    vector<string> fileVector;
-    string current;
-
-    if (!ifs.is_open()){
-        return -1;
-    }
-
-    ifs >> current;
-    fileVector.push_back(current);
-
-    while (!ifs.eof()) {
-        int i;
-        ifs >> current;
-
-        for (i = fileVector.size() - 1; i >= 0; --i) {
-            if (current >= fileVector[i]) {
-                fileVector.insert (fileVector.begin()+i+1, current);
-                break;
-            }
-            else if (i == 1) {
-                fileVector.insert (fileVector.begin(), current);
-            }
-        }
-    }
-    
-    for (int i = 0; i < fileVector.size(); ++i) {
-        cout << fileVector[i] << " ";
-    }
-
-    return 0;
-}
-
 int main() {
-    char fileName[20];
-    char sort;
+    string fileName;
 
     cout << "Enter File Name: ";
-    cin >> fileName;
-    cout << "Sort? (y/n) ";
-    cin  >> sort;
-    if(sort == 'y') {
+    getline(cin, fileName);
+
+    if(sortCheck()) {
         if (sortfile(fileName) < 0) {
             cout << "Failed to load file";
             return 0;
