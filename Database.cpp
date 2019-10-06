d7fef9494915c3ceb71d5a8dd1d35fed915c1273
James G Garcia <jimmygus22@gmail.com>
2019-10-05 20:51:36 -0500

    Update Database.cpp

--- Database.cpp
+++ Database.cpp
@@ -22,3 +22,31 @@ int main() {
 
     return 0;
 }
+#include "functions.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    string fileName;
+    int members;
+
+    cout << "Enter File Name: ";
+    getline(cin, fileName);
+
+    cout << "Enter # of memebers: ";
+    cin >> members;
+
+    if(sortCheck()) {
+        if (sortfile(fileName, members) < 0) {
+            cout << "Failed to load file";
+            return 0;
+        }
+    }
+
+    return 0;
+}
