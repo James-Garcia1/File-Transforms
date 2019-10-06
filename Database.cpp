2cdd542b5fae95a9965a71d213c253936d61d74e
James G Garcia <jimmygus22@gmail.com>
2019-10-06 12:29:56 -0500

    Update Database.cpp

--- Database.cpp
+++ Database.cpp
@@ -9,39 +9,26 @@ using namespace std;
 
 int main() {
     string fileName;
+    int members;
 
-    cout << "Enter File Name: ";
-    getline(cin, fileName);
-
-    if(sortCheck()) {
-        if (sortfile(fileName) < 0) {
+    if(methodCheck("Remove Duplicates")) {
+        cout << "Enter File Name: ";
+        cin.ignore();
+        getline(cin, fileName);
+        cout << "Enter # of memebers: ";
+        cin >> members;
+        if (removeDupes(fileName, members) < 0) {
             cout << "Failed to load file";
             return 0;
         }
     }
 
-    return 0;
-}
-#include "functions.h"
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <sstream>
-#include <vector>
-
-using namespace std;
-
-int main() {
-    string fileName;
-    int members;
-
-    cout << "Enter File Name: ";
-    getline(cin, fileName);
-
-    cout << "Enter # of memebers: ";
-    cin >> members;
-
-    if(sortCheck()) {
+    if(methodCheck("Sort")) {
+        cout << "Enter File Name: ";
+        cin.ignore();
+        getline(cin, fileName);
+        cout << "Enter # of memebers: ";
+        cin >> members;
         if (sortfile(fileName, members) < 0) {
             cout << "Failed to load file";
             return 0;
