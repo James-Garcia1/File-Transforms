4b3524389074c516f845c677d1aad8bad5203aab
James G Garcia <jimmygus22@gmail.com>
2019-10-05 20:52:14 -0500

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,13 +1,17 @@
 #include <string>
 #include <vector>
+#include <map>
 using namespace std;
 
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
-int sortfile(const string fileName);
-int writefile(const vector<string> &fileVector, string fileName,
-const unsigned int fileVectorSize, string fileop);
+int sortfile(const string fileName, const int numMems);
+int writefile(const vector<string> &fileVector, string fileName, 
+const unsigned int fileVectorSize, map<string, vector<string>> mem_map, 
+unsigned int numMems, string fileop);
 bool sortCheck();
+void memberLink(ifstream &ifs, const string current,
+ map<string, vector<string>> &mem_map, int numMems);
 
 #endif
