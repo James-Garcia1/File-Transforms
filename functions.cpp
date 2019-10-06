809a641899e374e15c6b72454bae0a998f285cd0
James G Garcia <jimmygus22@gmail.com>
2019-10-05 20:51:55 -0500

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -7,61 +7,22 @@
 
 using namespace std;
 
-int sortfile(const string fileName) {
-    ifstream ifs(fileName);
-    vector<string> fileVector;
-    string current;
+int main() {
+    string fileName;
+    int members;
 
-    if (!ifs.is_open()){
-        return -1;
-    }
+    cout << "Enter File Name: ";
+    getline(cin, fileName);
 
-    ifs >> current;
-    fileVector.push_back(current);
+    cout << "Enter # of memebers: ";
+    cin >> members;
 
-    while (!ifs.eof()) {
-        int i;
-        ifs >> current;
-
-        for (i = fileVector.size() - 1; i >= 0; --i) {
-            if (current >= fileVector[i]) {
-                fileVector.insert (fileVector.begin()+i+1, current);
-                break;
-            }
-            else if (i == 0) {
-                fileVector.insert (fileVector.begin(), current);
-            }
+    if(sortCheck()) {
+        if (sortfile(fileName, members) < 0) {
+            cout << "Failed to load file";
+            return 0;
         }
     }
-    
-    writefile(fileVector, fileName, fileVector.size(), "Sort");
 
     return 0;
-}
-
-int writefile(const vector<string> &fileVector, string fileName, const unsigned int fileVectorSize, string fileop) {
-    ofstream ofs(fileName.insert(0, fileop));
-
-    for (int i = 0; i < fileVectorSize; ++i) {
-        if (i < fileVectorSize - 1)
-            ofs << fileVector[i] << '\n';
-        else
-            ofs << fileVector[i];
-    }
-
-    return 0;
-}
-
-bool sortCheck() {
-    bool sort;
-    char userinput = 'o';
-    while (userinput != 'y' && userinput != 'n') {
-        cout << "Sort? (y/n) ";
-        cin >> userinput;
-    }
-
-    if (userinput == 'y')
-        return true;
-    else
-        return false;    
 }
